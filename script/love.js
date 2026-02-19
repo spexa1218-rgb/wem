5452a74c4b4e31c6bfd0ab610eaa3fd47225cff0
https <spexa1218@gmail.com>
2026-02-19 23:52:48 +0300

    Add files via upload

--- script/love.js
+++ script/love.js
@@ -41,7 +41,7 @@ function init() {
     ctx.font = `${fontSize}px Arial`;
     ctx.fillStyle = "lightblue";
     ctx.textAlign = "center";
-    ctx.fillText("как я люблю тебя", width / 2, height / 2 + (mobile ? 100 : 200));
+    ctx.fillText("🤍", width / 2, height / 2 + (mobile ? 100 : 200));
   }
 
   function heartPosition(rad) {
